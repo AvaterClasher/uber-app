ff079e1db3efe55bc5d9717262536c9b13b0d97c
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-05-01 00:26:38 +0530

    modified:   App.js 	new file:   components/NavOptions.js 	modified:   package-lock.json 	modified:   package.json 	new file:   screens/HomeScreen.js 	new file:   screens/MapScreen.js 	new file:   slices/navSlice.js 	new file:   store.js

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text style={styles.text}></Text>
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{
+              headerShown: false,
+            }}/>
+            <Stack.Screen name="MapScreen" component={MapScreen} options={{
+              headerShown: false,
+            }}/>
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    color:"#ff0000",
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "#ff0000",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  text:{
-    fontSize:40,
-  }
 });
---
+++ screens/MapScreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import tw from 'twrnc'
+
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>MapScreen</Text>
+    </View>
+  )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
